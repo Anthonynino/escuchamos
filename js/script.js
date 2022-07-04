2797cb6940b627222b539180878888b3c1a16135
Jose Miguel Garcia Quintero <josegarcia.developer@gmail.com>
2022-07-04 11:22:04 -0400

    First commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+window.sr = ScrollReveal();
+ 	
+
+ 	sr.reveal(".test", {
+ 		duration: 3000,
+ 		origin: "right",
+ 		distance: "-100px"
+ 	})
+
+ 	sr.reveal(".title", {
+ 		duration: 3000,
+ 		origin: "bottom",
+ 		distance: "-100px"
+ 	});
+
+ 	sr.reveal(".rightsc", {
+ 		duration: 3000,
+ 		origin: "right",
+ 		distance: "-100px"
+ 	});
+
+ 	sr.reveal(".leftsc", {
+ 		duration: 3000,
+ 		origin: "left",
+ 		distance: "-100px"
+ 	});
+
+ 	sr.reveal(".bottomsc", {
+ 		duration: 3000,
+ 		origin: "bottom",
+ 		distance: "-100px"
+ 	});
+ 	
